64ef247f3f2e12037e8b831b9ba0c0f402213cff
z <123>
2022-05-11 11:59:09 +0800

    nerf: gleam reflect damage

--- src/main/java/io/github/sefiraat/slimetinker/events/PlayerDamagedEvents.java
+++ src/main/java/io/github/sefiraat/slimetinker/events/PlayerDamagedEvents.java
@@ -376,12 +376,12 @@ public final class PlayerDamagedEvents {
     }
 
     public static void linksMetal(EventFriend friend) {
-        if (GeneralUtils.testChance(1, 5) && friend.getDamagingEntity() instanceof LivingEntity) {
+        if (GeneralUtils.testChance(1, 10) && friend.getDamagingEntity() instanceof LivingEntity) {
             friend.setCancelEvent(true);
             LivingEntity l = (LivingEntity) friend.getDamagingEntity();
             l.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 1));
             l.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, l.getLocation(), 3, 0.2, 0.2, 0.2);
-            l.damage(friend.getInitialDamage(), friend.getPlayer());
+            l.damage(friend.getInitialDamage() / 5, friend.getPlayer());
         }
     }
 
